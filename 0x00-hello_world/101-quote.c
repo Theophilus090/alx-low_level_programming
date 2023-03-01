a0e504a519f5e49c0ae5f11fb6bb0df8e2a1bd74
Theophilus090 <theophilusblessing090@gmail.com>
2023-03-01 12:13:13 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19", followed by a newline, to standard error.
- * Return: Always 1.
+ * Return: Always 0 (Success)
  */
 
 int main(void)
