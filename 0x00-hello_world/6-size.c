ad623296b9de371dd4959a702e89c09173236110
Theophilus090 <theophilusblessing090@gmail.com>
2023-03-01 08:38:04 -0800

    create 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- * main - prints the size of various types on the computer it is compiled and run on..
- * Return: Always 0.
+ * main - A program that prints a line using a printf function
+ * Return: 0 (Success)
  */
+
 int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
