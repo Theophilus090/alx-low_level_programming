b9bc65ccab05ca2ba334bffd1847304af694001f
Theophilus090 <theophilusblessing090@gmail.com>
2023-03-01 11:49:02 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line to the standard error 
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19", followed by a newline, to standard error.
  * Return 1 (Success)
  */
 
