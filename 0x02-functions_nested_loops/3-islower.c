d61efa5c0f4965a72aee8aadbbe27e2df86c75e0
Theophilus090 <theophilusblessing090@gmail.com>
2023-03-06 02:13:35 -0800

    create 3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lower character
+ * @c: The character to be checked
+ * Return: 1 for lower character or 0 for anythng else
+ */
+int _islower (int c)
+
+{
+	if (c >= 97 && c <= 122)
+	{
+	return (1);
+	}
+	return (0);
+}
